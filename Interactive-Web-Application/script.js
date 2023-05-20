7ca782b441f910634c98fda6a2ea0c659544954c
feyzadc <feyzadecdeli@hotmail.com>
2023-05-20 05:18:10 +0300

    feat: sprint 2 updated

--- Interactive-Web-Application/script.js
+++ Interactive-Web-Application/script.js
@@ -1,8 +1,49 @@
+// projects -- accordion
 
-// projects -- accordion JQuery UI
-$(function () {
-    $( "#accordion" ).accordion({
-        heightStyle: "content",
-        collapsible: true
-      });
+$(document).ready(function () {
+
+  $("#accordion").accordion({
+    heightStyle: "content",
+    collapsible: true
+  });
+
+});
+
+
+// about us -- read more
+
+$(document).ready(function () {
+
+  var time = 300;
+  $('.show-more').css('cursor', 'grab');
+
+  if ($('.ty-compact-list').length > 3) {
+    $('.ty-compact-list:gt(2)').hide(time);
+    $('.show-more').show(time);
+  }
+
+  $('.show-more').on('click', function () {
+    $('.ty-compact-list:gt(2)').toggle(time);
+    $(this).text() === 'Read less ...' ? $(this).text('Read more ...') : $(this).text('Read less ...');
+  });
+
+});
+
+
+// contact us -- submission alert
+
+$(document).ready(function () {
+  $('#dialog').hide();
+
+  $('form').submit(function (event) {
+
+    event.preventDefault();
+
+    $("#dialog").dialog({
+      open: function (event, ui) {
+        setTimeout("$('#dialog').dialog('close')", 4000);
+      }
+    });
+  });
+
 });

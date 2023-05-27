a6f10f39cf1b3d980e4b219fc20919555232bb0c
feyzadc <feyzadecdeli@hotmail.com>
2023-05-27 19:25:35 +0300

    feat: Activity-9 added

---
+++ Activity-9/Decdeli-Activity9.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+	// preload images
+	$("#image_list a").each(function () {
+		var swappedImage = new Image();
+		swappedImage.src = $(this).attr("href");
+	});
+
+	// set up event handlers for links    
+	$("#image_list a").click(function (evt) {
+
+		var imageURL = $(this).attr("href");
+		var caption = $(this).attr("title");
+		var time = 1000;
+
+		$("#caption").fadeOut(time, function () {
+			$("#caption").fadeIn(time);
+			$("#caption").text(caption);
+		});
+
+		$("#image").fadeOut(time, function () {
+			$("#image").attr("src", imageURL);
+			$("#image").fadeIn(time);
+		});	
+
+		// cancel the default action of the link
+		evt.preventDefault();
+	}); // end click
+
+	// move focus to first thumbnail
+	$("li:first-child a").focus();
+
+}); // end ready
